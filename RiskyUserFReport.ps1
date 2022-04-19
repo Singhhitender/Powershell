8471250aa982f8e639b57b19e1c0d1e732a0441f
singhhitender <singhhitender92@gmail.com>
2022-04-19 10:55:51 +0530

    Code to fetch risky user reports via graph

---
+++ RiskyUserFReport.ps1
@@ -0,0 +1,21 @@
+
+
+$ClientID      = ""        # Should be a ~36 hex character string; insert your info here
+$ClientSecret  = ""    # Should be a ~44 character string; insert your info here
+$tenantdomain  = "thecloudpro1.onmicrosoft.com"    # For example, contoso.onmicrosoft.com
+$loginURL      = "https://login.microsoft.com"
+$resource      = "https://graph.microsoft.com"
+$body      = @{grant_type="client_credentials";resource=$resource;client_id=$ClientID;client_secret=$ClientSecret}
+$oauth      = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+Write-Output $oauth
+if ($oauth.access_token -ne $null) {
+    $headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
+    $url = "https://graph.microsoft.com/v1.0/identityProtection/riskDetections"
+    Write-Output $url
+    $myReport = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $url)
+    foreach ($event in ($myReport.Content | ConvertFrom-Json).value) {
+        Write-Output $event
+    }
+} else {
+    Write-Host "ERROR: No Access Token"
+}
